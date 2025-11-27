84efe6c875540ba17d390aed52e8b0fa0b6df3c7
VUDUYDOAN <duydoanvu2004@gmail.com>
2025-11-28 00:27:21 +0700

    add SIEM INDEX and fix sleep time

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # --- SIEM Configuration ---
 SIEM_HOST = 'localhost'       
 SIEM_PORT = 9200
-SIEM_USER = ''                
-SIEM_PASS = ''                
-SIEM_INDEX = 'winlogbeat-*'   
+SIEM_USER = 'elastic'
+SIEM_PASS = '_L-LLMOjg1BKYRPxNc7h'             
+SIEM_INDEX = '.internal.alerts-security.alerts-default-*'
 
 # Protocol Configuration
-SIEM_USE_SSL = False          
+SIEM_USE_SSL = True          
 SIEM_VERIFY_CERTS = False     
 
 # --- Fuzzer Configuration ---
-CONSUMER_SLEEP_TIME = 60 
+CONSUMER_SLEEP_TIME = 90
